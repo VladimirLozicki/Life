be8ca71f98b38c33102692213ac99823b8e6a1e7
vladimirlozickiy <volodya199996@gmail.com>
2020-04-14 04:18:19 +0300

    update game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
-import java.util.Random;
-import java.util.jar.JarOutputStream;
-
-import sun.net.www.content.image.gif;
+import java.util.Arrays;
 
 /**
  * @author vladimirlozickiy
@@ -12,41 +9,35 @@ public class Game {
     private int i;
     private int j;
     private int area[][];
-    private static final int SIZE = 10;
+    private static final int SIZE = 30;
     private int a[] = new int[8];
     private int life = 0;
-    private int dead = 0;
- 
+
     Game(int i, int j) {
         this.i = i;
         this.j = j;
         area = new int[i][j];
-        initArea(area);
         setInitLocation();
     }
 
-    public Game run() {
-        for (int u = 1; u < i - 1; u++){
-            for(int o = 1; o < j - 1; o++){
+    public void run(int time) {
+        int temp = 0;
+        while (temp < time) {
+            oneCycle();
+            time++;
+        }
+    }
+
+    public void oneCycle() {
+        for (int u = 1; u < i - 1; u++) {
+            for (int o = 1; o < j - 1; o++) {
                 result(u, o);
-               // printArea(getArea());
+                printArea();
                 try {
-                    Thread.sleep(100); 
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                 }
-                //System.out.print("\033[H\033[2J");
-            }
-        }
-        return this;
-    }
-
-    /**
-     * init area
-     */
-    private void initArea(int area[][]) {
-        for (int k = 0; k < i; k++) {
-            for (int p = 0; p < j; p++) {
-                area[k][p] = 0;
+                System.out.print("\033[H\033[2J");
             }
         }
     }
@@ -54,10 +45,10 @@ public class Game {
     /**
      * print area
      */
-    public void printArea(int area[][]) {
-        for (int k = 0; k < i; k++) {
-            for (int p = 0; p < j; p++) {
-                System.out.print(area[k][p]);
+    public void printArea() {
+        for (int k = 0; k < this.i; k++) {
+            for (int p = 0; p < this.j; p++) {
+                System.out.print(getArea()[k][p]);
             }
             System.out.print("\n");
         }
@@ -66,15 +57,11 @@ public class Game {
     /**
      * print area
      */
-    public void bv(int area[][]) {
-        for (int k = 0; k < i; k++) {
-            for (int p = 0; p < j; p++) {
-                result(k, p);
-            }
-            System.out.print("\n");
+    public void printNeighbors() {
+        for (int k = 0; k < getA().length; k++) {
+            System.out.print(a[k]);
         }
     }
-
 
     /**
      * set begin location life
@@ -84,7 +71,6 @@ public class Game {
         while (count < SIZE) {
             int x = (int) (Math.random() * i);
             int y = (int) (Math.random() * j);
-
             if (getArea()[x][y] == 0) {
                 getArea()[x][y] = 1;
             } else {
@@ -100,47 +86,52 @@ public class Game {
      * @param a
      * @param b
      */
-    private void result(int a, int b) {
+    public void result(int a, int b) {
         getNeighboirs(a, b);
         checkNeighbors();
-        int value = getArea()[a][b];
-        if (value == 1 && (getLife() == 2 || getLife()==3)) {
-            cellBorn(a, b);
-         }else if(value == 1 && (getLife() < 2 || getLife() > 3)){
-            cellDead(a, b);
-        }else if (value == 0 && getLife() == 3) {
+        int value = area[a][b];
+        if (value == 0) {
+            if (getLife() == 2 || getLife() == 3) {
                 cellBorn(a, b);
+            }
+        } else {
+            if (getLife() < 2 || getLife() > 3) {
+                cellDead(a, b);
+            }
         }
+        life = 0;
     }
 
-    private void checkNeighbors() {
+    public void checkNeighbors() {
         for (int p : getA()
         ) {
-            if (p == 0) {
-                dead++;
-            } else {
+            if (p == 1) {
                 life++;
             }
         }
     }
 
     private void getNeighboirs(int i, int j) {
-        a[0] = getArea()[i - 1][j - 1];
-        a[1] = getArea()[i - 1][j];
-        a[2] = getArea()[i - 1][j + 1];
-        a[3] = getArea()[i][j + 1];
-        a[4] = getArea()[i + 1][j + 1];
-        a[5] = getArea()[i + 1][j];
-        a[6] = getArea()[i + 1][j - 1];
-        a[7] = getArea()[i][j - 1];
+        int count = 0;
+        while (count < 8) {
+            for (int t = i - 1; t <= i + 1; t++) {
+                for (int l = j - 1; l <= j + 1; l++) {
+                    if (i == t && j == l) {
+                        continue;
+                    }
+                    a[count] = area[t][l];
+                    count++;
+                }
+            }
+        }
     }
 
     public void cellDead(int i, int j) {
-        getArea()[i][j] = 0;
+        area[i][j] = 0;
     }
 
     public void cellBorn(int i, int j) {
-        getArea()[i][j] = 1;
+        area[i][j] = 1;
     }
 
     public int[] getA() {
@@ -151,21 +142,13 @@ public class Game {
         return life;
     }
 
-
-    public int getDead() {
-        return dead;
-    }
-
     public int[][] getArea() {
         return area;
     }
 
     public static void main(String[] args) throws IOException {
-        Game game = new Game(5, 5);
-        game.printArea(game.getArea());
-        game.run();
-        System.out.println();
-        game.printArea(game.getArea());
+        Game game = new Game(10, 10);
+        game.run(3);
+        game.printArea();
     }
-
 }
